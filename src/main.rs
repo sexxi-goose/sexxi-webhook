79666809fbcc34f7f928639f3e9ee28679f0e848
Azhng <archer.xn@gmail.com>
2020-06-04 01:35:17 -0400

    webhook: support rust test

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 extern crate base64;
+extern crate pretty_env_logger;
+#[macro_use] extern crate log;
 extern crate hyper;
 extern crate hyper_tls;
 extern crate serde_json;
@@ -24,20 +26,20 @@ const REVIEWER: &'static str = "sexxi-bot";
 // Should be located in $HOME/.secrets/.gh
 const SEXXI_SECRET: &'static str = "SEXXI_SECRET";
 const SEXXI_USERNAME: &'static str = "sexxi-bot";
-//const SEXXI_GIT_DIR: &'static str = "$HOME/scratch/sexxi-rust/.git";
-//const SEXXI_WORK_TREE: &'static str = "$HOME/scratch/sexxi-rust";
-//const SEXXI_PROJECT: &'static str = "rust";
-const SEXXI_GIT_DIR: &'static str = "$HOME/src/github.com/webhook-test/.git";
-const SEXXI_WORK_TREE: &'static str = "$HOME/src/github.com/webhook-test";
-const SEXXI_PROJECT: &'static str = "webhook-test";
+const SEXXI_GIT_DIR: &'static str = "$HOME/scratch/sexxi-rust/.git";
+const SEXXI_WORK_TREE: &'static str = "$HOME/scratch/sexxi-rust";
+const SEXXI_PROJECT: &'static str = "rust";
+//const SEXXI_GIT_DIR: &'static str = "$HOME/src/github.com/webhook-test/.git";
+//const SEXXI_WORK_TREE: &'static str = "$HOME/src/github.com/webhook-test";
+//const SEXXI_PROJECT: &'static str = "webhook-test";
 
 const SEXXI_REMOTE_HOST: &'static str = "sorbitol";
 const SEXXI_LOG_FILE_DIR: &'static str = "www/build-logs";
 
 const BUILD_LOG_BASE_URL: &'static str = "https://csclub.uwaterloo.ca/~z577zhan/build-logs";
 
-const COMMENT_JOB_START: &'static str = "Start running build job...";
-const COMMENT_JOB_DONE: &'static str = "Job Completed";
+const COMMENT_JOB_START: &'static str = ":running_man: Start running build job";
+const COMMENT_JOB_DONE: &'static str = "✅ Job Completed";
 
 #[derive(Debug)]
 struct JobDesc {
@@ -92,11 +94,13 @@ async fn post_comment(comment: String, pr_number: u64) -> Result<(), String> {
             let client = Client::builder().build::<_, hyper::Body>(connector);
             let result = client.request(req).await;
             match result {
-                Ok(_resp) => {
-                    //println!("resp: {:#?}", &resp);
-                    Ok(())
+                Ok(resp) => {
+                    match resp.status() {
+                        StatusCode::CREATED => Ok(()),
+                        _ => Err(format!("http request error: {:#?}", resp)),
+                    }
                 },
-                Err(e) => Err(format!("http request to comment failed: {}", e))
+                Err(e) => Err(format!("http request failure: {}", e))
             }
         }
         Err(e) => Err(format!("failed fetch secret for http request: {}", e))
@@ -110,7 +114,7 @@ fn bad_req() -> Response<Body> {
 }
 
 
-fn remote_cmd(args: Vec<&str>, output: &mut File) -> Result<(), String> {
+fn remote_cmd(args: &mut Vec<&str>, output: &mut File) -> Result<(), String> {
     // TODO(azhng): let's finger cross this works.
     let output_file = output.try_clone().unwrap();
     let error_file = output.try_clone().unwrap();
@@ -132,19 +136,10 @@ fn remote_cmd(args: Vec<&str>, output: &mut File) -> Result<(), String> {
 fn remote_git_cmd(args: &mut Vec<&str>, output: &mut File) -> Result<(), String> {
     let mut git_cmd = vec![
         "git",
-        "--git-dir", SEXXI_GIT_DIR,
-        "--work-tree", SEXXI_WORK_TREE,
+        "-C", SEXXI_WORK_TREE,
     ];
     git_cmd.append(args);
-    remote_cmd(git_cmd, output)
-}
-
-async fn job_failure_handler<T: std::fmt::Display>(msg: &str, job: &JobDesc, log_file_path: &str, err: T) {
-    // TODO(azhng): format the buid log url
-    let err_msg = format!("job failed for sha {}, log file path {:#?}: {}: {}", &job.sha, log_file_path, msg, err);
-    println!("{}", &err_msg);
-    post_comment(err_msg, job.pr_num).await.expect("Ok");
-    println!("Ack job finished");
+    remote_cmd(&mut git_cmd, output)
 }
 
 fn remote_git_reset_branch(output: &mut File) -> Result<(), String> {
@@ -153,7 +148,7 @@ fn remote_git_reset_branch(output: &mut File) -> Result<(), String> {
 }
 
 fn remote_git_fetch_upstream(output: &mut File) -> Result<(), String> {
-    let mut cmd = vec!["fetch", "-a"];
+    let mut cmd = vec!["fetch", "--all"];
     remote_git_cmd(&mut cmd, output)
 }
 
@@ -162,8 +157,10 @@ fn remote_git_checkout_sha(sha: &str, bot_ref: &str, output: &mut File) -> Resul
     remote_git_cmd(&mut cmd, output)
 }
 
-// TODO(azhng): ...
-fn remote_git_rebase(sha: &str) {}
+fn remote_git_rebase_upstream(output: &mut File) -> Result<(), String> {
+    let mut cmd = vec!["rebase", "upstream/master"];
+    remote_git_cmd(&mut cmd, output)
+}
 
 fn remote_git_push(bot_ref: &str, output: &mut File) -> Result<(), String> {
     let mut cmd = vec!["push", "origin", bot_ref, "-f"];
@@ -175,72 +172,97 @@ fn remote_git_delete_branch(bot_ref: &str, output: &mut File) -> Result<(), Stri
     remote_git_cmd(&mut cmd, output)
 }
 
+fn remote_test_rust_repo(output: &mut File) -> Result<(), String> {
+    let mut cmd = vec!["cd", SEXXI_WORK_TREE, ";", "./x.py", "test", "-i", "-j32"];
+    remote_cmd(&mut cmd, output)
+}
 
-async fn run_and_build(job: JobDesc) {
-    let job_id = Uuid::new_v4();
+async fn job_failure_handler<T: std::fmt::Display>(msg: &str, job_id: &Uuid, job: &JobDesc, err: T) {
+    let err_msg = format!("❌ Build job {} failed, access build log [here]({}/{}): {}: {}",
+    job_id, BUILD_LOG_BASE_URL, job_id, msg, err);
+    error!("{}", &err_msg);
+    if let Err(e) = post_comment(err_msg, job.pr_num).await {
+        warn!("unable to post comment for failed job: {}", e);
+    } else {
+        info!("Ack job finished");
+    }
+}
+
+async fn run_and_build(job_id: Uuid, job: JobDesc) {
+    // TODO(azhng): figure out how to perform additional cleanup.
+
     let log_file_name = format!("{}/{}/{}", env::var("HOME").unwrap(), SEXXI_LOG_FILE_DIR, &job_id);
     let log_file_path = Path::new(&log_file_name);
-    println!("Creating log file at: {}", &log_file_name);
+    info!("Creating log file at: {}", &log_file_name);
     let mut log_file = File::create(&log_file_path).unwrap();
     let bot_ref = format!("bot-{}", &job.head_ref);
 
     if let Err(e) = remote_git_reset_branch(&mut log_file) {
-        job_failure_handler("unable to reset branch", &job, &*log_file_name, e).await;
+        job_failure_handler("unable to reset branch", &job_id, &job, e).await;
         return;
     }
 
     if let Err(e) = remote_git_fetch_upstream(&mut log_file) {
-        job_failure_handler("unable to fetch upstream", &job, &log_file_name, e).await;
+        job_failure_handler("unable to fetch upstream", &job_id, &job, e).await;
         return;
     }
 
     if let Err(e) = remote_git_checkout_sha(&job.sha, &bot_ref, &mut log_file) {
-        job_failure_handler("unable to check out sha", &job, &log_file_name, e).await;
+        job_failure_handler("unable to check out commit", &job_id, &job, e).await;
         return;
     }
 
-    // TODO(azhng): run test
+    if let Err(e) = remote_git_rebase_upstream(&mut log_file) {
+        job_failure_handler("unable to rebase against upstream", &job_id, &job, e).await;
+        return;
+    }
+
+    if let Err(e) = remote_test_rust_repo(&mut log_file) {
+        remote_git_reset_branch(&mut log_file).expect("Ok");
+        remote_git_delete_branch(&bot_ref, &mut log_file).expect("Ok");
+        job_failure_handler("unit test failed", &job_id, &job, e).await;
+        return;
+    }
 
     if let Err(e) = remote_git_push(&bot_ref, &mut log_file) {
-        job_failure_handler("unable to push bot branch", &job, &log_file_name, e).await;
+        job_failure_handler("unable to push bot branch", &job_id, &job, e).await;
         return;
     }
 
     if let Err(e) = remote_git_reset_branch(&mut log_file) {
-        job_failure_handler("unable to reset branch for clean up", &job, &log_file_name, e).await;
+        job_failure_handler("unable to reset branch for clean up", &job_id, &job, e).await;
         return;
     }
 
     if let Err(e) = remote_git_delete_branch(&bot_ref, &mut log_file) {
-        job_failure_handler("unable to delete bot branch", &job, &log_file_name, e).await;
+        job_failure_handler("unable to delete bot branch", &job_id, &job, e).await;
         return;
     }
 
-    let msg = format!("{}, access build log [here]({}/{})",
-    COMMENT_JOB_DONE, BUILD_LOG_BASE_URL, &job_id);
-    println!("{}", &msg);
-    post_comment(msg, job.pr_num).await.unwrap();
-    println!("Ack job finished");
+    let msg = format!("{}, access build log [here]({}/{})", COMMENT_JOB_DONE, BUILD_LOG_BASE_URL, &job_id);
+    info!("{}", &msg);
+    if let Err(e) = post_comment(msg, job.pr_num).await {
+        warn!("failed to post comment for job completion: {}", e);
+    } else {
+        info!("Ack job finished");
+    }
 }
 
 
 async fn start_build_job(job: JobDesc) {
-    if job.reviewer == REVIEWER && job.action == REVIEW_REQUESTED {
-        match post_comment(String::from(COMMENT_JOB_START), job.pr_num).await {
-            Ok(_) => {}
-            Err(e) => {
-                println!("Failed to start job for sha {}: {}", &job.sha, e);
-                return;
-            }
+    let job_id = Uuid::new_v4();
+    let comment = format!("{}, job id: {}", COMMENT_JOB_START, job_id);
+    match post_comment(comment, job.pr_num).await {
+        Ok(_) => {}
+        Err(e) => {
+            error!("Failed to start job for sha {}: {}", &job.sha, e);
+            return;
         }
-        tokio::spawn(async move {
-            println!("Kicking of job for sha: {}", &job.sha);
-            run_and_build(job).await;
-        });
-    } else {
-        println!("Webhook rescieved: skipped due to unsatisfied conditions:");
-        println!("\tjob: {:#?}", &job);
     }
+    tokio::spawn(async move {
+        info!("Kicking of job for sha: {}", &job.sha);
+        run_and_build(job_id, job).await;
+    });
 }
 
 async fn handler(req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
@@ -252,26 +274,36 @@ async fn handler(req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
 
             match blob {
                 Ok(json) => {
-                    let pr_number = json["pull_request"]["number"].as_u64().unwrap();
-                    let sha = json["pull_request"]["head"]["sha"].as_str().unwrap();
                     let reviewer = json["requested_reviewer"]["login"].as_str().unwrap();
-                    let head_ref = json["pull_request"]["head"]["ref"].as_str().unwrap();
                     let action = json["action"].as_str().unwrap();
 
-                    let job = JobDesc{
-                        action: String::from(action),
-                        reviewer: String::from(reviewer),
-                        sha: String::from(sha),
-                        pr_num: pr_number,
-                        head_ref: String::from(head_ref),
-                    };
+                    if reviewer == REVIEWER {
+                        match action {
+                            REVIEW_REQUESTED => {
+                                let pr_number = json["pull_request"]["number"].as_u64().unwrap();
+                                let sha = json["pull_request"]["head"]["sha"].as_str().unwrap();
+                                let head_ref = json["pull_request"]["head"]["ref"].as_str().unwrap();
 
-                    // TODO(azhng): track the jobs somehow.
-                    start_build_job(job).await;
+                                let job = JobDesc{
+                                    action: String::from(action),
+                                    reviewer: String::from(reviewer),
+                                    sha: String::from(sha),
+                                    pr_num: pr_number,
+                                    head_ref: String::from(head_ref),
+                                };
+
+                                // TODO(azhng): track the jobs somehow.
+                                start_build_job(job).await;
+                            },
+                            _ => {
+                                warn!("Action: {} not handled", action);
+                            }
+                        }
+                    }
                     Ok::<_, hyper::Error>(Response::default())
                 }
                 Err(e) => {
-                    println!("parsing error: {}", e);
+                    error!("parsing error: {}", e);
                     Ok::<_, hyper::Error>(bad_req())
                 }
             }
@@ -295,21 +327,21 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let addr = ([127, 0, 0, 1], 55420).into();
     let server = Server::bind(&addr).serve(make_svc);
 
-    println!("Listening to http://{}", addr);
-
-    println!(r"Server Ready. Configuration:
+    info!("Listening to http://{}", addr);
+    info!(r"Server Ready. Configuration:
     SEXXI_USERNAME: {},
     SEXXI_WORK_TREE: {},
     SEXXI_GIT_DIR: {},
     SEXXI_PROJECT: {},
     SEXXI_REMOTE_HOST: {},
     SEXXI_LOG_FILE_DIR: {},
-    BUILD_LOG_BASE_URL: {}", SEXXI_USERNAME, SEXXI_WORK_TREE,
+    BUILD_LOG_BASE_URL: {}",
+    SEXXI_USERNAME, SEXXI_WORK_TREE,
     SEXXI_GIT_DIR, SEXXI_PROJECT, SEXXI_REMOTE_HOST,
     SEXXI_LOG_FILE_DIR, BUILD_LOG_BASE_URL);
 
     if let Err(err) = server.await {
-        eprintln!("server error: {}", err);
+        error!("server error: {}", err);
     }
 
     Ok(())
